33566d9813016edbc667916594ff34d26c00552a
Karishmacode <karishma1502@gmail.com>
2026-05-29 19:36:10 +0530

    .env and db file change

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,8 +1,18 @@
+// 
+
+
+
 import mongoose from "mongoose";
+import dns from "dns";
+
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 30000,
+    });
+
     console.log("MongoDB Connected");
   } catch (error) {
     console.log("DB Error:", error);
